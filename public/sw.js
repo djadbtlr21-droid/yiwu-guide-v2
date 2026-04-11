a897c8a97902749902f41cad2c5a018fa4e620d9
djadbtlr21-droid <djadbtlr21@gmail.com>
2026-04-11 17:39:09 +0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,20 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
+const CACHE_NAME = 'yiwu-guide-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['/']);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', () => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
